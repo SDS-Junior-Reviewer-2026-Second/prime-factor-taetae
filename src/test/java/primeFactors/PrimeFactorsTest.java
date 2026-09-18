eeede01aa7618c6a72ea5c6550bcf92f6bd3cf71
Taegon21 <aksen5240@gmail.com>
2026-09-18 10:13:39 +0900

    feat: init project

---
+++ src/test/java/primeFactors/PrimeFactorsTest.java
@@ -0,0 +1,14 @@
+package primeFactors;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimeFactorsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
