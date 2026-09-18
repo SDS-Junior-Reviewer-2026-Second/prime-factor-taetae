7f042974e4c75d2e85e1e0bc470ae4eda080da86
Taegon21 <aksen5240@gmail.com>
2026-09-18 11:02:00 +0900

    feat: PrimeFactor 구현

--- src/main/java/primeFactors/PrimeFactors.java
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -1,5 +1,48 @@
 package primeFactors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
 
+    public List<Integer> factor(int number) {
+        List<Integer> factors = new ArrayList<>();
+
+        // early return(예외처리)
+        if (number < 2) {
+            return factors;
+        }
+
+        for (int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                factors.add(i);
+                number /= i;
+            }
+        }
+
+        return factors;
+    }
+
+    // Optimized Algorithm of Prime Factorization
+    public List<Integer> optimizedFactor(int n) {
+        List<Integer> factors = new ArrayList<>();
+
+        while (n % 2 == 0) {
+            factors.add(2);
+            n /= 2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 2) {
+            factors.add(n);
+        }
+
+        return factors;
+    }
 }
--- src/test/java/primeFactors/PrimeFactorsTest.java
+++ src/test/java/primeFactors/PrimeFactorsTest.java
@@ -1,14 +1,42 @@
 package primeFactors;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PrimeFactorsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	void testPrimeFactorOf1() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		assertEquals(Arrays.asList(), primeFactors.factor(1));
 	}
+
+	@Test
+	void testPrimeFactorOf6() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		assertEquals(Arrays.asList(2,3), primeFactors.factor(6));
+	}
+
+	@Test
+	void testPrimeFactorOf12() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		assertEquals(Arrays.asList(2,2,3), primeFactors.factor(12));
+	}
+
+	@Test
+	void testPrimeFactor420() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		assertEquals(Arrays.asList(2,2,3,5,7), primeFactors.factor(420));
+	}
+
+	// Optimized Factorization Tests
+	@Test
+	void testOptimizedFactorOf12() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		assertEquals(Arrays.asList(2,2,3), primeFactors.optimizedFactor(12));
+	}
+
+
 
 }
